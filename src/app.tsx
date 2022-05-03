51a45dc1792ffc29b75b89de5834b431306b2998
MildaPau <pauzaite.milda@gmail.com>
2022-05-03 17:22:21 +0300

    fix: admin route

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import LandingPageLayout from './components/landing-page-layout';
+import LandingPageLayoutLogin from './components/landing-page-layout -login';
 import HomePage from './pages/home-page';
 import HomeworkPage from './pages/homework-page';
 import AboutPage from './pages/about-page';
@@ -14,16 +15,21 @@ import { AuthProvider } from './features/auth/auth-context';
 const App: React.FC = () => (
   <AuthProvider>
     <Routes>
+      <Route path="/" element={<LandingPageLayoutLogin />}>
+        <Route path="admin" element={<LoginPage />} />
+      </Route>
+    </Routes>
+    <Routes>
       <Route path="/" element={<LandingPageLayout />}>
         <Route index element={<HomePage />} />
         <Route path="homework" element={<HomeworkPage />} />
         <Route path="about" element={<AboutPage />} />
         <Route path="gallery" element={<GalleryPage />} />
         <Route path="contact" element={<ContactPage />} />
-        <Route path="wp-admin" element={<LoginPage />} />
         <Route path="auth/register" element={<RegisterPage />} />
       </Route>
     </Routes>
+
   </AuthProvider>
 
 );
