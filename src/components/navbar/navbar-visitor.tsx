d9d5a78c0aaec6b80c1d250d8a36d461f94b7727
MildaPau <pauzaite.milda@gmail.com>
2022-05-25 21:26:11 +0300

    fix: delete sculpture link from navbar

--- src/components/navbar/navbar-visitor.tsx
+++ src/components/navbar/navbar-visitor.tsx
@@ -7,11 +7,7 @@ const NavbarVisitorMenu: React.FC = () => (
     <NavbarLink to="/about">About</NavbarLink>
     <NavbarLink to="/gallery">Gallery</NavbarLink>
     <NavbarLink to="/contact">Contact</NavbarLink>
-    <NavbarLink to="/sculpture">Sculpture</NavbarLink>
   </>
 );
 
 export default NavbarVisitorMenu;
-
-/* <NavbarLink to="/auth/login">Login</NavbarLink>
-<NavbarLink to="/auth/register">Register</NavbarLink> */
