679fb98acaadd6cd328844667fa53f4f422fa4e6
MildaPau <pauzaite.milda@gmail.com>
2022-05-27 13:09:12 +0300

    feat: form validation

--- src/pages/create-new-sculpture-page/create-new-sculpture-form.tsx
+++ src/pages/create-new-sculpture-page/create-new-sculpture-form.tsx
@@ -4,23 +4,37 @@ import {
 import { useFormik, FormikConfig } from 'formik';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import * as Yup from 'yup';
 import ButtonScale from '../../components/button-scale';
 import { createNewSculptureAction } from '../../store/action-creators';
 import { useRootDispatch } from '../../store/hooks';
 import { CreateSculpture } from '../../types/create-sculpture';
 
+type CreateConfig = FormikConfig<CreateSculpture>;
+
+const initialValues: CreateSculpture = {
+  img: '',
+  title: '',
+  year: '',
+  dimensions: '',
+};
+
+const validationSchema: Yup.SchemaOf<CreateSculpture> = Yup.object({
+  title: Yup.string()
+    .required('This field is Required'),
+  year: Yup.string()
+    .matches(/^[0-9]*$/, 'Only numbers is alowed')
+    .required('This field is Required'),
+  dimensions: Yup.string()
+    .required('This field is Required'),
+  img: Yup.string()
+    .required('This field is Required')
+    .matches(/https?:\/\/(www\.)?/, 'Enter correct url'),
+});
+
 const CreateNewSculptureForm: React.FC = () => {
   const navigate = useNavigate();
   const dispatch = useRootDispatch();
-
-  type CreateConfig = FormikConfig<CreateSculpture>;
-
-  const initialValues: CreateSculpture = {
-    img: '',
-    title: '',
-    year: '',
-    dimensions: '',
-  };
 
   const handleSubmitCreateSculpture: CreateConfig['onSubmit'] = ({
     title, year, dimensions, img,
@@ -32,10 +46,11 @@ const CreateNewSculptureForm: React.FC = () => {
   };
 
   const {
-    values, handleSubmit, handleChange,
+    values, handleSubmit, handleChange, handleBlur, touched, errors,
   } = useFormik<CreateSculpture>({
     initialValues,
     onSubmit: handleSubmitCreateSculpture,
+    validationSchema,
   });
 
   return (
@@ -70,40 +85,72 @@ const CreateNewSculptureForm: React.FC = () => {
           label="Title"
           value={values.title}
           onChange={handleChange}
+          onBlur={handleBlur}
           variant="outlined"
           fullWidth
           sx={{ mt: 3 }}
         />
+        <Typography sx={{
+          fontSize: 12, color: 'red', mt: 1, ml: 1,
+        }}
+        >
+          {touched.title && errors.title}
+        </Typography>
+
         <TextField
           name="year"
           type="text"
           label="Year"
           value={values.year}
           onChange={handleChange}
+          onBlur={handleBlur}
           variant="outlined"
           fullWidth
           sx={{ mt: 3 }}
         />
+        <Typography sx={{
+          fontSize: 12, color: 'red', mt: 1, ml: 1,
+        }}
+        >
+          {touched.year && errors.year}
+        </Typography>
+
         <TextField
           name="dimensions"
           type="text"
           label="Dimensions"
           value={values.dimensions}
           onChange={handleChange}
+          onBlur={handleBlur}
           variant="outlined"
           fullWidth
           sx={{ mt: 3 }}
         />
+        <Typography sx={{
+          fontSize: 12, color: 'red', mt: 1, ml: 1,
+        }}
+        >
+          {touched.dimensions && errors.dimensions}
+        </Typography>
+
         <TextField
           name="img"
           type="text"
           label="Image"
           value={values.img}
           onChange={handleChange}
+          onBlur={handleBlur}
           variant="outlined"
           fullWidth
           sx={{ mt: 3 }}
         />
+        <Typography sx={{
+          fontSize: 12, color: 'red', mt: 1, ml: 1,
+        }}
+        >
+          {touched.img && errors.img}
+        </Typography>
+
         <Box sx={{ display: 'flex', justifyContent: 'space-between', mt: 3 }}>
           <ButtonScale type="submit">Create</ButtonScale>
           <ButtonScale
